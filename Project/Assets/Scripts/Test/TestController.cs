c16b051e220c3d83cf939f740e2c1a53e77f65c0
FlDeLima <florian.de_lima@hotmail.com>
2017-05-16 13:51:15 -0400

    MainProject

---
+++ Project/Assets/Scripts/Test/TestController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class TestController : MonoBehaviour {
+	
+	[SerializeField]	GameObject TestObj;
+	[SerializeField]	GameObject Cercle;
+
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.Space))
+		{
+			if (!Cercle.activeSelf)
+				Cercle.SetActive(true);
+			else if(!TestObj.activeSelf)
+				TestObj.SetActive(true);
+			else
+			{
+				Cercle.SetActive(false);
+				TestObj.SetActive(false);
+			}
+		}
+	}
+}
